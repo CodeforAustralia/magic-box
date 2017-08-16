0e598d3b8d6aab36a76b1ca13219a3cfb5ab7302
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-08-16 10:40:15 +1000

    Prep for first showcase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Dashboard&nbsp;&nbsp;<i class="fa fa-magic" aria-hidden="true"></i>
+
+                    <i class="icon-magic"></i></div>
+
+                <div class="panel-body">
+                    <a href="#">Your Details</a>
+                </div>
+                <div class="panel-body">
+                    <a href="#">Letters</a>
+                </div>
+                <div class="panel-body">
+                    <a href="#">Status on the Hogwarts Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/letters', function () {
+  if (Auth::check()) {
+      $reference_id = Auth::user()->reference_id;
+      $letters = DB::table('letters')->where('reference_id', $reference_id)->get();
+#      return $letters;
+      return view('letters', compact ('letters'));
+    } else {
+      return view('welcome');
+    }
+});
